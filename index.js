7bcd27cea3aef3eff9bc720dfcc370bc42fd76ad
ngocchien <ngocchien.1711@gmail.com>
2019-06-14 23:16:31 +0700

    Update error message

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
-    await context.sendActivity(error);
     await context.sendActivity(`Oops. Something went wrong!`);
+    await context.sendActivity(error);
 };
 
 // Create the main dialog.
